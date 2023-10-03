1c1e312b3ad294d19ffca4ab885f51ba2db63e4e
zahirulislam15 <zahirulislam15@gmail.com>
2023-10-03 20:05:19 +0600

    is changed?

--- Assignment 4/Product.php
+++ Assignment 4/Product.php
@@ -1,4 +1,5 @@
 <?php
+
 class Product {
     public $id;
     public $name;
@@ -14,22 +15,21 @@ class Product {
 
     public function getFormattedPrice($p){
         $price1 = number_format($p,2);
-        // echo ($price1);
-        return $price1;
+        return $price1; 
     }
 
     public function showDetails(){
         echo "ID: ". $this->id . "\n";
         echo "Name: ". $this->name . PHP_EOL;
         $prices = $this->getFormattedPrice($this->price);
-        echo "Price: ". $prices;
+        printf("Price: %.2f", $prices);
 
     }
 }
 
 
 // Test the Product class
-$product = new Product(1, 'T-shirt', 19.999999);
+$product = new Product(1, 'T-shirt', 19.99);
 $product->showDetails();
 
 
