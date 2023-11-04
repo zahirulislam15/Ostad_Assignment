3e6450c4dc2db8a420435738e3ccfc53abb3af9b
zahirulislam15 <zahirulislam15@gmail.com>
2023-11-04 23:53:49 +0600

    Assignment 5 task 4

---
+++ Assignment 6/task4.php
@@ -0,0 +1,12 @@
+SELECT
+    C.name AS customer_name,
+    SUM(O.total_amount) AS total_purchase_amount
+FROM
+    Customers C
+JOIN
+    Orders O ON C.customer_id = O.customer_id
+GROUP BY
+    C.name
+ORDER BY
+    total_purchase_amount DESC
+LIMIT 5;
