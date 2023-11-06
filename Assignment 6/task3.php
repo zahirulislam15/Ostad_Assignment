0fa4012ea24700b25398eadb36fc902dfe81b284
zahirulislam15 <zahirulislam15@gmail.com>
2023-11-06 16:01:29 +0600

    Task 3

--- Assignment 6/task3.php
+++ Assignment 6/task3.php
@@ -1,3 +1,9 @@
 Task 3:
 
 
+SELECT c.name AS category_name, SUM(oi.quantity * oi.unit_price) AS total_revenue
+FROM Categories c
+INNER JOIN Products p ON c.category_id = p.category_id
+LEFT JOIN Order_Items oi ON p.product_id = oi.product_id
+GROUP BY c.name
+ORDER BY total_revenue DESC;
